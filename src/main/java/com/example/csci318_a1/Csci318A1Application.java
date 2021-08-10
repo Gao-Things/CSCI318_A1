7c54e150eb0a28439d15c317b79e758c197b3a0a
Gao-Things <949731988@qq.com>
2021-08-10 14:12:10 +0800

    1.0
    first commit

---
+++ src/main/java/com/example/csci318_a1/Csci318A1Application.java
@@ -0,0 +1,13 @@
+package com.example.csci318_a1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Csci318A1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Csci318A1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/csci318_a1/Csci318A1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.csci318_a1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Csci318A1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
